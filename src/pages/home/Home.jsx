b7c26abd07f901d40d41aa2769ab4b3c78bbc537
Sarman Keshvala <sagarkeshwala15@gmail.com>
2023-03-14 08:16:48 +0530

    added categories slider on home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import CatCard from "../../components/catCard/CatCard";
 import Featured from "../../components/featured/Featured";
+import Slide from "../../components/slide/Slide";
 import TrustedBy from "../../components/trustedBy/TrustedBy";
+import { cards } from "../../data";
 
 const Home = () => {
 	return (
 		<div>
 			<Featured />
 			<TrustedBy />
+			<Slide slidesToShow={5} arrowsScroll={1}>
+				{cards.map((card) => (
+					<CatCard key={card.id} item={card} />
+				))}
+			</Slide>
 		</div>
 	);
 };
