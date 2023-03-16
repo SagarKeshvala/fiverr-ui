3a2e657e1c95233475ba913413e2e4b1d4ba4a8a
Sarman Keshvala <sagarkeshwala15@gmail.com>
2023-03-16 08:14:24 +0530

    added projects data on home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import CatCard from "../../components/catCard/CatCard";
 import Featured from "../../components/featured/Featured";
+import ProjectCard from "../../components/projectCard/ProjectCard";
 import Slide from "../../components/slide/Slide";
 import TrustedBy from "../../components/trustedBy/TrustedBy";
-import { cards } from "../../data";
+import { cards, projects } from "../../data";
 import "./Home.scss";
 
 const Home = () => {
@@ -96,6 +97,11 @@ const Home = () => {
 					</div>
 				</div>
 			</div>
+			<Slide slidesToShow={4} arrowsScroll={1}>
+				{projects.map((card) => (
+					<ProjectCard key={card.id} item={card} />
+				))}
+			</Slide>
 		</div>
 	);
 };
